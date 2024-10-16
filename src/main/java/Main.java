d769e9030759db2884a9364745911b6b3e778cb8
dominic@mxc.org <dominic@mxc.org>
2024-10-16 17:16:48 +0200

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,26 @@
 import com.google.gson.Gson;
-import com.dampcake.bencode.Bencode; - available if you need it!
+// import com.dampcake.bencode.Bencode; - available if you need it!
 
 public class Main {
   private static final Gson gson = new Gson();
-
   public static void main(String[] args) throws Exception {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
     String command = args[0];
     if("decode".equals(command)) {
-          String bencodedValue = args[1];
-          String decoded;
-          try {
-            decoded = decodeBencode(bencodedValue);
-          } catch(RuntimeException e) {
-            System.out.println(e.getMessage());
-            return;
-          }
-          System.out.println(gson.toJson(decoded));
-
+       String bencodedValue = args[1];
+       String decoded;
+       try {
+         decoded = decodeBencode(bencodedValue);
+       } catch(RuntimeException e) {
+         System.out.println(e.getMessage());
+         return;
+       }
+       System.out.println(gson.toJson(decoded));
     } else {
       System.out.println("Unknown command: " + command);
     }
-
   }
-
   static String decodeBencode(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
       int firstColonIndex = 0;
